9f6820759eab37e68988e076d4e207f8d6eeaaac
ASPILGI <elysion715@gmail.com>
2022-03-18 20:36:09 +0900

    [기초-리스트]
    [기초-리스트]

---
+++ codeup6097.py
@@ -0,0 +1,22 @@
+h,w = map(int,input().split())
+
+list = []
+for i in range(h+1) :
+    list.append([])
+    for j in range(w+1) :
+        list[i].append(0)
+
+n = int(input())
+for i in range(n) :
+    l,d,x,y = input().split()
+    if int(d)==0 :
+        for j in range(int(l)):
+            list[int(x)][int(y)+j] = 1
+    else :
+        for j in range(int(l)) :
+            list[int(x)+j][int(y)] = 1
+
+for i in range(1, h+1) :
+    for j in range(1, w+1) :
+        print(list[i][j], end=' ')
+    print()
